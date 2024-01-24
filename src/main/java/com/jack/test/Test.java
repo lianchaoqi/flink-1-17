c3e1da946183aec66b7a7693a90c599a241bb23a
jack <2504810905@qq.com>
2024-01-25 01:01:48 +0800

    ceshi tijiao

---
+++ src/main/java/com/jack/test/Test.java
@@ -0,0 +1,15 @@
+package com.jack.test;
+
+/**
+ * @BelongsProject: flink-1-17
+ * @BelongsPackage: com.jack.test
+ * @Author: lianchaoqi
+ * @CreateTime: 2024-01-25  00:59
+ * @Description: ~~~~
+ * @Version: jdk1.8
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
